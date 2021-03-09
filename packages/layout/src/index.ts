551194659e2e812f76ef6d3b42ac7a1fcc14f6b7
Patrick Krawczykowski <patrick@dreadful.design>
2021-03-08 21:13:31 -0600

    update build config

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Component(props: any) {
-  return;
+  const [open, setOpen] = useState(false);
+  useEffect(() => {}, []);
+  return open;
 }
