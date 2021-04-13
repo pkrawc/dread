59a4325b96999a077749623ea14c038b1d40d3fd
Patrick Krawczykowski <patrick@dreadful.design>
2021-04-12 21:37:34 -0500

    fix: add container size theme prop

--- packages/core/src/container.tsx
+++ packages/core/src/container.tsx
@@ -1,5 +1,6 @@
 import css from "@styled-system/css"
 import styled from "styled-components"
+import { system } from "styled-system"
 import { sx, SXObject } from "./dread-provider"
 
 interface ContainerProps {
@@ -7,6 +8,10 @@ interface ContainerProps {
 }
 
 export const Container = styled.section<ContainerProps>(
-  css({ maxWidth: "80rem", mx: "auto", px: "2rem" }),
+  css({
+    maxWidth: (theme) => (theme.containerSize ? theme.containerSize : "80rem"),
+    mx: "auto",
+    px: "2rem",
+  }),
   sx
 )
