29155fd0e086296d5e7ff2ca822f0a4e00da8fe5
Patrick Krawczykowski <patrick@dreadful.design>
2021-08-05 09:12:06 -0500

    update tests

--- packages/controls/controls.test.ts
+++ packages/controls/controls.test.ts
@@ -1,1 +1,31 @@
 // Haha, made you look.
+
+describe("Controls Tests", () => {
+  it("<Button />", () => {
+    expect(true).toBe(true)
+  })
+
+  it("<Checkbox />", () => {
+    expect(true).toBe(true)
+  })
+
+  it("<Chip />", () => {
+    expect(true).toBe(true)
+  })
+
+  it("<Input />", () => {
+    expect(true).toBe(true)
+  })
+
+  it("<Radio />", () => {
+    expect(true).toBe(true)
+  })
+
+  it("<Slider />", () => {
+    expect(true).toBe(true)
+  })
+
+  it("<Switch />", () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ packages/utils/utils.test.ts
@@ -0,0 +1,19 @@
+// Haha, made you look.
+
+describe("Utils Tests", () => {
+  it("wrapEvent", () => {
+    expect(true).toBe(true)
+  })
+
+  it("mergeRefs", () => {
+    expect(true).toBe(true)
+  })
+
+  it("closestScrollable", () => {
+    expect(true).toBe(true)
+  })
+
+  it("getFocusable", () => {
+    expect(true).toBe(true)
+  })
+})
